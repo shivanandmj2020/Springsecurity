4c60dc79aed64ffb20ad53231bdaf9768948ed08
Shivanand Mj <Shivanand.Mj@godigit.com>
2024-04-30 21:07:25 +0530

    first commit

---
+++ src/main/java/com/shiva/spring/security/api/controller/ApplicationController.java
@@ -0,0 +1,16 @@
+package com.shiva.spring.security.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/auth")
+public class ApplicationController {
+
+	@GetMapping("/msg")
+	public String greeting() {
+		return "spring security example";
+	}
+
+}
